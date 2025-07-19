c3a59c5cb1aa2d375a2f42f26267d8e3e6797898
Jayanth-reflex <jayanthreddy268.jr@gmail.com>
2025-07-19 21:05:04 +0530

    chore: commit all changes before redeploying

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import "./App.css";
 export default function App() {
   return (
     <div className="center-container">
-      <header className="header">
+      {/* <header className="header">
         <span role="img" aria-label="rocket">🚀</span>
         <span className="header-title">Jayanth's GitHub Repos</span>
         <span role="img" aria-label="sparkles">✨</span>
-      </header>
+      </header> */}
       <Carousel />
     </div>
   );
