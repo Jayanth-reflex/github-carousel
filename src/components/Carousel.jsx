d68da0136024c461f3ee89e48bdaa888efd12bb1
Jayanth-reflex <jayanthreddy268.jr@gmail.com>
2025-07-19 22:14:23 +0530

    refactor: Rebuild carousel to match original CSS-only implementation, no JS transforms

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,50 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Carousel.scss';
 
-const GITHUB_USER = 'Jayanth-reflex';
+const cards = Array.from({ length: 8 }, (_, i) => ({
+  index: i + 1,
+  title: `Image ${8 - i}`,
+  desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac sapien leo. Proin rutrum magna eu aliquam vulputate. Nullam suscipit.'
+}));
 
 export default function Carousel() {
-  const [repos, setRepos] = useState([]);
-  const [isHovered, setIsHovered] = useState(false);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${GITHUB_USER}/repos`)
-      .then(res => res.json())
-      .then(data => setRepos(data.slice(0, 8)));
-  }, []);
-
   return (
-    <div
-      className="carousel"
-      style={{
-        transform: 'perspective(1000px) rotateX(70deg)',
-        animationPlayState: isHovered ? 'paused' : 'running',
-      }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-    >
-      {repos.map((repo, i) => (
-        <div className={`card c${i+1}`} key={repo.id}>
-          <a
-            href={repo.html_url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card-link"
-          >
-            <div
-              className="img"
-              style={{
-                backgroundImage: `url(https://picsum.photos/300/180.webp?random=${i+1})`,
-                backgroundSize: 'cover'
-              }}
-            />
-            <p className="repo-name">{repo.name}</p>
-          </a>
-          <span className="repo-desc">{repo.description || 'No description'}</span>
+    <div className="carousel">
+      {cards.map(card => (
+        <div className={`card c${card.index}`} key={card.index}>
+          <div
+            className="img"
+            style={{
+              backgroundImage: `url(https://picsum.photos/300/300.webp?random=${card.index})`,
+              backgroundSize: '190px 190px'
+            }}
+          />
+          <p>{card.title}</p>
+          <span>{card.desc}</span>
         </div>
       ))}
-      {repos.map((repo, i) => (
-        <div className={`cardb cb${i+1}`} key={`b${repo.id}`}></div>
+      {cards.map(card => (
+        <div className={`cardb cb${card.index}`} key={`b${card.index}`}></div>
       ))}
     </div>
   );
